2f4abe72441c637487e551ee8b548035f20051c6
HIMMEL95 <bbluesky7738@gmail.com>
2021-09-15 15:32:23 +0900

    first commit

--- GraduationProject/app/src/main/java/com/example/graduationproject/MainActivity.kt
+++ GraduationProject/app/src/main/java/com/example/graduationproject/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.graduationproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.SearchView
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- test/app/src/main/java/com/example/test/MainActivity.kt
+++ test/app/src/main/java/com/example/test/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        val menuItem = menu.findItem(R.id.app_bar_search)
+        val searchView = menuItem.actionView as SearchView
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String?): Boolean {
+                //TODO("Not yet implemented")
+                return true
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                //TODO("Not yet implemented")
+                return true
+            }
+        })
+        return true
     }
 }
