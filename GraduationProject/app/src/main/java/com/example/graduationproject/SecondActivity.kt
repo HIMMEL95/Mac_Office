9f3e34c56b2204c8b0a4c9218f332a8d39252aa9
HIMMEL95 <bbluesky7738@gmail.com>
2021-09-15 20:04:54 +0900

    first commmit

---
+++ GraduationProject/app/src/main/java/com/example/graduationproject/SecondActivity.kt
@@ -0,0 +1,24 @@
+package com.example.graduationproject
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.location)
+
+        // action bar back button
+        val actionBar = supportActionBar
+        actionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    // action bar back button func
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Toast.makeText(applicationContext, "Back Button Clicked", Toast.LENGTH_SHORT).show()
+        return super.onOptionsItemSelected(item)
+    }
+
+}
--- test/app/src/main/java/com/example/test/MainActivity.kt
+++ test/app/src/main/java/com/example/test/MainActivity.kt
@@ -3,12 +3,26 @@ package com.example.test
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.SearchView
+import androidx.fragment.app.Fragment
+import androidx.appcompat.app.ActionBar
+import android.widget.Toast
+
+import androidx.annotation.NonNull
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // action bar back intent
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        startActivity(Intent(this@MainActivity, SecondActivity::class.java))
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
